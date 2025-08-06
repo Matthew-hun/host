26201c00f275702de40435a88886f1c890733410
Matthew-hun <majdianmate.dev@gmail.com>
2025-08-06 15:11:15 +0200

    host

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // basePath és assetPrefix NEM KELL root domainhez!
   eslint: {
     ignoreDuringBuilds: true,
   },
