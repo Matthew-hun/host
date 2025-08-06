542fdddf2689bce7c822338d0ce0b579ed9d082f
Matthew-hun <majdianmate.dev@gmail.com>
2025-08-06 14:35:58 +0200

    CSS javítás assetPrefix módosítással

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  assetPrefix: '/',
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
