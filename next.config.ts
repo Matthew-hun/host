6fdee9ea125ebcb360b5913c0eacb825631b3f5e
Matthew-hun <majdianmate.dev@gmail.com>
2025-08-06 14:34:12 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  assetPrefix: './',
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
+
