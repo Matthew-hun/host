6e10b43d578a0fa09931d9a094e562120aacee20
Matthew-hun <majdianmate.dev@gmail.com>
2025-08-13 14:24:44 +0200

    Update ScoreInput.tsx

--- app/components/ScoreInput.tsx
+++ app/components/ScoreInput.tsx
@@ -38,6 +38,10 @@ const ScoreInput = () => {
     }
   };
 
+    useEffect(() => {
+    inputRef.current?.focus();
+  }, [match]); // vagy más releváns függőség
+
   const handleInputChange = (value: string) => {
     setInputValue(value);
   };
